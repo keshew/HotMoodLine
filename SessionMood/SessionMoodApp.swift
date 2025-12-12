e77fd712d8251a02e3534316f981d976953faee7
keshew <artyom.korotckov2017@yandex.ru>
2025-12-12 14:32:51 +0500

    Initial Commit

---
+++ SessionMood/SessionMoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SessionMoodApp.swift
+//  SessionMood
+//
+//  Created by Артём Коротков on 12.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SessionMoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
